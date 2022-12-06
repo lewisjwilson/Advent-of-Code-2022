52b4af347990734019fa24adeddca3789e9d3b10
Lewis Wilson <55784291+lewisjwilson@users.noreply.github.com>
2022-12-06 12:33:33 +0900

    Create Part 1.py

---
+++ Day 5/Part 1.py
@@ -0,0 +1,95 @@
+# Question
+
+# --- Day 5: Supply Stacks ---
+# The expedition can depart as soon as the final supplies have been unloaded from the ships. Supplies are stored in stacks of marked crates, 
+# but because the needed supplies are buried under many other crates, the crates need to be rearranged.
+# 
+# The ship has a giant cargo crane capable of moving crates between stacks. To ensure none of the crates get crushed or fall over, 
+# the crane operator will rearrange them in a series of carefully-planned steps. After the crates are rearranged, the desired crates will be at the top of each stack.
+# 
+# The Elves don't want to interrupt the crane operator during this delicate procedure, but they forgot to ask her which crate will end up where, 
+# and they want to be ready to unload them as soon as possible so they can embark.
+# 
+# They do, however, have a drawing of the starting stacks of crates and the rearrangement procedure (your puzzle input). For example:
+# 
+#     [D]    
+# [N] [C]    
+# [Z] [M] [P]
+#  1   2   3 
+# 
+# move 1 from 2 to 1
+# move 3 from 1 to 3
+# move 2 from 2 to 1
+# move 1 from 1 to 2
+# In this example, there are three stacks of crates. Stack 1 contains two crates: crate Z is on the bottom, and crate N is on top. 
+# Stack 2 contains three crates; from bottom to top, they are crates M, C, and D. Finally, stack 3 contains a single crate, P.
+# 
+# Then, the rearrangement procedure is given. In each step of the procedure, a quantity of crates is moved from one stack to a different stack. 
+# In the first step of the above rearrangement procedure, one crate is moved from stack 2 to stack 1, resulting in this configuration:
+# 
+# [D]        
+# [N] [C]    
+# [Z] [M] [P]
+#  1   2   3 
+# In the second step, three crates are moved from stack 1 to stack 3. Crates are moved one at a time, so the first crate to be moved (D) 
+# ends up below the second and third crates:
+# 
+#         [Z]
+#         [N]
+#     [C] [D]
+#     [M] [P]
+#  1   2   3
+# Then, both crates are moved from stack 2 to stack 1. Again, because crates are moved one at a time, crate C ends up below crate M:
+# 
+#         [Z]
+#         [N]
+# [M]     [D]
+# [C]     [P]
+#  1   2   3
+# Finally, one crate is moved from stack 1 to stack 2:
+# 
+#         [Z]
+#         [N]
+#         [D]
+# [C] [M] [P]
+#  1   2   3
+# The Elves just need to know which crate will end up on top of each stack; in this example, the top crates are C in stack 1, M in stack 2, 
+# and Z in stack 3, so you should combine these together and give the Elves the message CMZ.
+# 
+# After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+
+
+# Solution
+
+# (no. of crates, from stack, to stack)
+instructions = [(3, 2, 9),
+(1, 1, 6),
+...
+(1, 7, 4),
+(6, 7, 5)]
+
+stacks = {1: ["T", "D", "W", "Z", "V", "P"],
+    2: ["L", "S", "W", "V", "F", "J", "D"],
+    3: ["Z", "M", "L", "S", "V", "T", "B", "H"],
+    4: ["R", "S", "J"],
+    5: ["C", "Z", "B", "G", "F", "M", "L", "W"],
+    6: ["Q", "W", "V", "H", "Z", "R", "G", "B"],
+    7: ["V", "K", "P", "C", "B", "D", "N"],
+    8: ["P", "T", "B", "Q"],
+    9: ["H", "G", "Z", "R", "C"]
+    }
+
+for instruction in instructions:
+    crates = instruction[0]
+    from_stack = instruction[1]
+    to_stack = instruction[2]
+    for c in range(crates):
+        removed = stacks[from_stack].pop()
+        stacks[to_stack].append(removed)
+
+final_text = ""
+for stack in stacks:
+    final_text += stacks[stack].pop()
+    
+print(final_text)
